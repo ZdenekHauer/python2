a5c9025d3027168468ca0ca273253fad6977dc44
ZdenekHauer <zdenek.hauer@infis.cz>
2025-03-19 14:38:03 +0100

    Add files via upload

---
+++ ukol2.py
@@ -0,0 +1,16 @@
+a = int(input("Zadejte první číslo: "))
+b = int(input("Zadejte druhé číslo: "))
+
+print(f"Součet: {a + b:.2f}")
+print(f"Rozdíl: {a - b:.2f}")
+print(f"Součin: {a * b:.2f}")
+
+if b != 0:
+    print(f"Podíl: {a / b:.2f}")
+else:
+    print("Podíl: Nulou nelze dělit")
+
+
+
+
+
